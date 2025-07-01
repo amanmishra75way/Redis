60a6fd9f4a64369346ab6e4e4d1de00e5dac7bc3
amanmishra75way <amishra.75way@gmail.com>
2025-07-01 10:43:39 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"));
+
+app.listen(5000, () => console.log("Server started on port 5000"));
