70b20f603b8a06aa5cd22f889d2df5f64bcdea4f
amanmishra75way <amishra.75way@gmail.com>
2025-07-01 11:46:57 +0530

    Redis setup example while server

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -7,3 +7,20 @@ redisClient.on("connect", () => console.log("Redis connected"));
 
 redisClient.connect();
 module.exports = redisClient;
+
+// to be used while hosted redis on server
+// const redis = require("redis");
+// require("dotenv").config();
+
+// const redisClient = redis.createClient({
+//   url: process.env.REDIS_URL,
+// });
+
+// redisClient.on("error", (err) => console.error("Redis Error:", err));
+// redisClient.on("connect", () => console.log("Connected to Redis"));
+
+// redisClient.connect();
+
+// module.exports = redisClient;
+
+// Add this to env REDIS_URL=rediss://default:<password>@your-redis-endpoint.upstash.io:6379
